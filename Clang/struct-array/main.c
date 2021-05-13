a7b36127c038477ec11e32fea926b6099882f7ef
Oscaner Miao <oscaner1997@gmail.com>
2021-05-13 12:55:17 +0800

    Clang: struct-array.

---
+++ Clang/struct-array/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct {
+  int amount;
+  char *name;
+} coins [] = {
+  {1, "penny"},
+  {5, "nickel"},
+  {10, "dime"},
+  {25, "quarter"},
+  {50, "half-dollar"}
+};
+
+int main() {
+
+  int k = 10;
+
+  for (int i = 0; i < sizeof(coins) / sizeof(coins[0]); i++) {
+    if (k == coins[i].amount) {
+      printf("%s\n", coins[i].name);
+    }
+  }
+
+  return -1;
+}
